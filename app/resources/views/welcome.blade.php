af27a45ee1921afbb01e404320535223b1cd0f79
Gun <59160244@go.buu.ac.th>
2019-05-15 00:42:05 +0700

    Add welcome page

--- app/resources/views/welcome.blade.php
+++ app/resources/views/welcome.blade.php
@@ -3,97 +3,141 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="description" content="">
+		<meta name="author" content="">
 
         <title>Laravel</title>
-
-        <!-- Fonts -->
+		
+		<!-- Bootstrap Core CSS -->
+		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        
+		<!-- CSS -->
+		<link href="{{ asset('css/landing-page.css') }}" rel="stylesheet">
+		
+		<!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+			
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
+	
+	<!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top topnav" role="navigation">
+        <div class="container topnav">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand topnav" href="#">หน้าแรก</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    @if (Route::has('login'))      
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
-
+						<li>
+                        <a href="{{ route('login') }}" class="btn btn-default btn-lg">Login</a>
+						</li>
                         @if (Route::has('register'))
+							<li>
                             <a href="{{ route('register') }}">Register</a>
+							</li>
                         @endif
-                    @endauth
-                </div>
+                    @endauth      
             @endif
+                </ul>
+            </div>
+            <!-- /navbar-collapse -->
+        </div>
+        <!-- /container -->
+    </nav>
+	
+	<a name="about"></a>
+    <div class="intro-header">
+        <div class="container">
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="intro-message">
+                        <h1>GameRoom</h1>
+                        <h3>Game</h3>
+                        <hr class="intro-divider">
+                    </div>
                 </div>
+            </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+        </div>
+        <!-- /container -->
+    </div>
+	
+	<!-- Page Content -->
+
+	<a  name="services"></a>
+    <div class="content-section-a">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5 col-sm-6">
+                    <hr>
+                    <div class="clearfix"></div>
+                    <h2 class="section-heading">Find Games</h2>
+                    <p class="lead">--<br>--</p>
+                </div> 
+            </div>
+        </div>
+        <!-- /.container -->
+
+    </div>
+    <!-- /.content-section-a -->
+
+    <div class="content-section-b">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5 col-lg-offset-1 col-sm-push-6  col-sm-6">
+                    <hr>
+                    <div class="clearfix"></div>
+					<h2 class="section-heading">Save Chapters</h2>
+                    <p class="lead">--<br>--</p>
+                </div>            
+            </div>
+        </div>
+        <!-- /.container -->
+
+    </div>
+    <!-- /.content-section-b -->
+    <!-- /.content-section-a -->
+
+	
+	<!-- Footer -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <ul class="list-inline">
+                        <li>
+                            <a href="#">หน้าแรก</a>
+                        </li>
+                        <li class="footer-menu-divider">&sdot;</li>
+                        <li>
+                           <a href="#contact">ติดต่อเรา</a>
+                        </li>
+                        <li class="footer-menu-divider">&sdot;</li>
+                        <li>
+						   <a href="https://github.com/laravel/laravel">GitHub</a>    
+                        </li>
+                    </ul>
+                  <!--  <p class="copyright text-muted small">Copyright &copy; ZuGeTor. All Rights Reserved</p>  -->
                 </div>
             </div>
         </div>
+    </footer>
+
     </body>
 </html>
